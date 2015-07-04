f37566f55851b23d79fcdce6fbdcabeef5317c3f
Jessé Pereira de Souza Toledo <jesse_hacker@hotmail.com>
2015-07-04 17:00:34 -0300

    Infraestrutura GetConnection SqlServer

---
+++ Infraestrutura.Tests/SqlServer/TestConexaoSqlServer.cs
@@ -0,0 +1,33 @@
+﻿using Infraestrutura.SqlServer;
+using NUnit.Framework;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Infraestrutura.Tests.SqlServer
+{
+    [TestFixture]
+    public class TestConexaoSqlServer
+    {
+        private SqlConnection Conexao;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Conexao = ConexaoSqlServer.GetConnection();
+        }
+
+        [Test]
+        public void ObjetoDeConexaoFoiCriado()
+        {
+            Assert.IsNotNull(Conexao);
+        }
+
+        [Test]
+        public void ConexaoFoiAberta()
+        {
+            Assert.DoesNotThrow(Conexao.Open);
+            Assert.IsTrue(Conexao.State == ConnectionState.Open);
+        }
+
+    }
+}
---
+++ Infraestrutura/SqlServer/ConexaoSqlServer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace Infraestrutura.SqlServer
+{
+    public class ConexaoSqlServer
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"server=JESSÉ\SQLEXPRESS;database = Estudos; Trusted_Connection=Yes;");
+        }
+    }
+}
